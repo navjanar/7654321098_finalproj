d7210d37e3624ad84a389503827b37dedf045a6d
naveen janarthanan <navjanar@gmail.com>
2018-03-07 23:27:17 -0800

    Fix #3

--- server.R
+++ server.R
@@ -6,6 +6,7 @@ library(shiny)
 library(DT)
 library(rsconnect)
 library(shinycssloaders)
+library(lubridate)
 
 my.server <- function(input, output) {
   data <- read.csv("data/movies.csv")
@@ -14,31 +15,56 @@ my.server <- function(input, output) {
     check_lang <- input$lang
     rating_max <- input$rating[2]
     rating_min <- input$rating[1]
-    
-    
+    check_year <- input$year
+    data$release_date <- lubridate::mdy(data$release_date)
     curr_data <- filter(data, original_language == check_lang) %>% 
       filter(vote_average <= rating_max) %>% filter(vote_average >= rating_min) %>% 
-      filter(grepl(input$genre, genres) == TRUE)
+      filter(grepl(input$genre, genres) == TRUE) 
+    if (check_year != "") {
+      curr_data <- filter(curr_data, year(release_date) == check_year)
+    }
+    
+    if(input$action != 0) {
+      num <- nrow(curr_data)
+      if (num > 10) {
+        num <- 10
+      }
+      curr_data <- sample_n(curr_data, num)
+    }
     num <- nrow(curr_data)
     if (num > 10) {
       num <- 10
     }
-    curr_data <<- sample_n(curr_data, num)
+    curr_data <- sample_n(curr_data, num)
   })
   
-  
   output$table <- renderDataTable({
-    display_data <- select(changing_data(), c("original_title", "release_date", "overview", "runtime"))
+    DT::datatable((select(changing_data(), c("original_title", "overview", "runtime", "vote_average"))), 
+                  options = list(paging = FALSE))
   }, rownames = FALSE)
   
   output$plot <- renderPlot({
-    plot_data <- subset(changing_data(), budget != 0 & revenue != 0)
-    ggplot(plot_data, aes(x=budget, y=revenue, label=title)) +
-      labs(y= "Revenue", x = "Budget") +
+    plot_data <- filter(changing_data(), budget != 0) %>% filter(revenue != 0)
+    validate(
+      need(nrow(plot_data) != 0, "No budget/revenue data to plot.")
+    )
+    ggplot(plot_data, aes((x = budget), y = (revenue/1000000), label=title)) +
+      labs(y = "Revenue ($ in millions)", x = "Budget") +
+      #geom_point()+
       geom_text(aes(label=title),hjust=-.1, vjust=.3) +
       ggtitle("Budget vs Revenue Comparison") + 
       theme(plot.title = element_text(face="bold", size=32, hjust=0)) +
-      theme(axis.title = element_text(face="bold", size=22))
+      theme(axis.title = element_text(face="bold", size=22)) +
+      theme(panel.background = element_rect(fill = "lightblue",
+                                            colour = "lightblue",
+                                            size = 0.5, linetype = "solid"))
+  })
+  
+  output$gather_info <- renderText({
+    paste("Data gathered from the movie data set found of kaggle")
+  })
+  output$our_info <- renderText({
+    paste("Created by: Naveen Janarthanan, Camille Munro, Andy Tillotson, Jose Santos")
   })
 }
 
--- ui.R
+++ ui.R
@@ -1,8 +1,12 @@
 # Final Project
 library(shinycssloaders)
+library(shinythemes)
+
 
 my.ui <- fluidPage(
-  titlePanel("Random Movie Generator"),
+  theme = shinytheme("cerulean"),
+  titlePanel("Random Movie Generator",
+  windowTitle = "Random Movie Generator"), 
   
   sidebarPanel(
     selectInput("lang", h3("Pick a language:"), choices = c("Chinese" = "zh", "English" = "en", "French" = "fr", "German" = "de", 
@@ -11,9 +15,10 @@ my.ui <- fluidPage(
     selectInput("genre", h3("Pick a genre:"), choices = c("Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", 
                                                           "Family", "Fantasy", "Foreign", "History", "Horror", "Mystery", "Romance",  
                                                           "Science Fiction", "Thriller", "TV Movie", "War", "Western")),
-    # textInput("year", label = h3("Year released"), value = "2018"), 
+    textInput("year", label = h3("Year released"), value = ""), 
     sliderInput("rating", label = h3("Rating:"), min = 0,
                 max = 10, value = c(0, 10)),
+    actionButton("action", label = "Refresh"),
     width = 3
   ),
   
@@ -21,7 +26,11 @@ my.ui <- fluidPage(
     withSpinner(DT::dataTableOutput('table')),
     br(),
     br(),
-    withSpinner(plotOutput('plot'))
+    withSpinner(plotOutput('plot')),
+    br(), 
+    br(), 
+    textOutput("gather_info"),
+    textOutput("our_info")
   ) 
 )
 
